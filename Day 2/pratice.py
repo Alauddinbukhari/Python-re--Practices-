ffff5d11e7a5120a8e7137d7fdb490df626ca3a5
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2024-07-31 11:48:12 -0600

    done with day 2 practice

---
+++ Day 2/pratice.py
@@ -0,0 +1,47 @@
+# two_digit_number = input()
+#
+# print(type(two_digit_number))
+#
+#
+# print(two_digit_number[0])
+# print(two_digit_number[1])
+#  #indexing string
+#
+#
+# a= float(123)
+#
+# b=56
+#
+#
+# print(a+b)
+# +-/*
+# %
+# **
+# PEMDAS
+
+#BMI PROJECT
+
+# weight= int(input("Please insert weight in kilograms"))
+#
+# height = int(input("Please insert height in meters"))
+#
+# bmi= weight/(height**2) *10000
+#
+# print(bmi)
+#
+# 76
+
+
+#life in weeks
+
+age = int(input())
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+weeks_in_a_year= 52
+weeks_till_your_age= 52* age
+weeks_till_90_age = 4680
+left_weeks=weeks_till_90_age - weeks_till_your_age
+
+print(f"You have {left_weeks} weeks left.")
+
+score=score+1 == score+=1
